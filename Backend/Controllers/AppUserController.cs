6cde1226d26db8d1f2ebb68b6b306638336907ec
Stephane Latil <stephanelatil@gmail.com>
2024-06-25 13:55:36 +0200

    Adds User identity and controllers

---
+++ Backend/Controllers/AppUserController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppUserController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public AppUserController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AppUser
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/AppUser/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetAppUser(string id)
+        {
+            var appUser = await _context.Users.FindAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return appUser;
+        }
+
+        // PUT: api/AppUser/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppUser(string id, AppUser appUser)
+        {
+            if (id != appUser.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(appUser).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AppUser
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<AppUser>> PostAppUser(AppUser appUser)
+        {
+            _context.Users.Add(appUser);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (AppUserExists(appUser.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetAppUser", new { id = appUser.Id }, appUser);
+        }
+
+        // DELETE: api/AppUser/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppUser(string id)
+        {
+            var appUser = await _context.Users.FindAsync(id);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(appUser);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AppUserExists(string id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,7 +1,10 @@
 using Backend.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 
@@ -11,23 +14,94 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //insert DBContext
-builder.Services.AddDbContext<ApplicationDBContext>(opt =>
-    {
-        opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-    });
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<ApplicationDBContext>(opt =>
+        {
+            opt.UseInMemoryDatabase("SaveMailDev");
+        });
+}
+else
+{
+    builder.Services.AddDbContext<ApplicationDBContext>(opt =>
+        {
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+            if (connectionString.Length < "localhost".Length)
+            {
+                //TODO: use env variables here
+            }
+            opt.UseNpgsql(connectionString);
+        });
+}
+//Setup SendGrid
+//string a = builder.Configuration.GetRequiredSection("SendGrid")['SendGridKey'];
+
+builder.Services.AddProblemDetails();
+// Add User auth
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<ApplicationDBContext>().AddDefaultTokenProviders();
+
+//Removes need for email confimation (no email server yet)
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Lockout.AllowedForNewUsers=false;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(20);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.SignIn.RequireConfirmedEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    // Secure enabled if login done over https otherwise http is accepted
+    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    // Inaccessible by JS
+    options.Cookie.HttpOnly = true;
+    // 5 days
+    options.ExpireTimeSpan = TimeSpan.FromDays(10);
+    // Ensure Cookie is auto-renewed if any authenticated call is made 5-10 days after last refresh
+    options.SlidingExpiration = true;
+});
 
 var app = builder.Build();
+
+// Add the user of authenticated users
+app.UseAuthorization();
+
+// Map user model
+app.MapIdentityApi<AppUser>();
+
+//In case of 4xx error so it doesn't leak info
+app.UseStatusCodePages(async context =>
+{
+    context.HttpContext.Response.ContentType = "application/json";
+    var result = $"{{'message':'{context.HttpContext.Response.StatusCode}'}}";
+    await context.HttpContext.Response.WriteAsync(result);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
+}else
+{
+    // in case of 5xx error to avoid leaking info
+    app.UseExceptionHandler(errorApp =>
+    {
+        errorApp.Run(async context =>
+        {
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            string result = "{'message':'500: Internal Server Error'}";
+            await context.Response.WriteAsync(result);
+        });
+    });
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
