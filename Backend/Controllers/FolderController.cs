965b3f1f094276603bcefbf6d6312363eb4c6cec
Stephane Latil <stephanelatil@gmail.com>
2024-07-05 14:08:32 +0200

    Adds getting paginated emails in folder

--- Backend/Controllers/FolderController.cs
+++ Backend/Controllers/FolderController.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Backend.Models;
 using Microsoft.AspNetCore.Identity;
 using Backend.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
@@ -19,14 +14,17 @@ namespace Backend.Controllers
         private readonly ApplicationDBContext _context;
         private readonly UserManager<AppUser> _userManager;
         private readonly IFolderService _folderService;
+        private readonly IPaginationService<IOrderedQueryable<Mail>, Mail> _paginatorService;
 
         public FolderController(ApplicationDBContext context,
                                 UserManager<AppUser> userManager,
-                                IFolderService folderService)
+                                IFolderService folderService,
+                                IPaginationService<IOrderedQueryable<Mail>, Mail> paginatorService)
         {
             this._context = context;
             this._userManager = userManager;
             this._folderService = folderService;
+            this._paginatorService = paginatorService;
         }
 
         // GET: api/Folder/5
@@ -46,6 +44,29 @@ namespace Backend.Controllers
                 return this.Forbid();
 
             return folder;
+        }
+
+        // GET: api/Folder/5
+        [HttpGet("{id}/Mails")]
+        [Authorize]
+        public async Task<ActionResult<PaginatedList<Mail>>> GetMails(long id, PaginationQueryParameters paginationQueryParameters)
+        {
+            var folder = await this._context.Folder.FindAsync(id);
+
+            if (folder == null)
+            {
+                return this.NotFound();
+            }
+            
+            var self = await this._userManager.GetUserAsync(this.User);
+            if (self is null || folder?.MailBox?.Owner != self)
+                return this.Forbid();
+
+            return await this._paginatorService.GetPageAsync(
+                            (IOrderedQueryable<Mail>)folder.Mails.OrderByDescending(m =>m.DateReceived),
+                            this.Request.Path.Value ?? "",
+                            paginationQueryParameters.PageNumber,
+                            paginationQueryParameters.PageSize);
         }
 
         // DELETE: api/Folder/5
