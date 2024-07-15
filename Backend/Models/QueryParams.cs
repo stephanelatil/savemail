610d22f6ad19e3b78f15df3a93c86fee60afa306
Stephane Latil <stephanelatil@gmail.com>
2024-07-15 14:47:43 +0200

    Now correct QueryParams

--- Backend/Models/QueryParams.cs
+++ Backend/Models/QueryParams.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Backend.Models
 {
     public class PaginationQueryParameters
     {
         const int maxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
         private int pageSize = 25;
+
+        [FromQuery(Name = "pageNumber")]
+        [Range(1, int.MaxValue, ErrorMessage = "Page number must be greater than 0.")]
+        public int PageNumber { get; set; } = 1;
+
+        [FromQuery(Name = "pageSize")]
+        [Range(1, maxPageSize, ErrorMessage = "Page size must be between 1 and 50.")]
         public int PageSize
         {
             get { return this.pageSize; }
