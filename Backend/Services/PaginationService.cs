2951978f22c5df9d49715308176083e193cb3a7b
Stephane Latil <stephanelatil@gmail.com>
2024-07-15 14:52:22 +0200

    Edits pagination service to static

--- Backend/Services/PaginationService.cs
+++ Backend/Services/PaginationService.cs
@@ -1,35 +1,43 @@
-using Backend.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
 namespace Backend.Services
 {
-    public interface IPaginationService<T,U> where T : IOrderedQueryable<U> where U : class
+    public class PaginationService
     {
-        public Task<PaginatedList<U>> GetPageAsync(T querySet, string route, int page, int pageSize);
-    }
-
-    public class QueryParamTakeSkipPaginationService<T, U> : IPaginationService<T, U> where T : IOrderedQueryable<U> where U : class
-    {
-        /// <summary>
-        /// Returns a PaginatedList gotten from the given queryset. 
-        /// </summary>
-        /// <param name="querySet">The queryset to paginate. NOTE: it should already be ordered-by whatever key you require.</param>
-        /// <param name="route">The query URL (without query params)</param>
-        /// <param name="page">The page number</param>
-        /// <param name="pageSize">The max number of element of the list</param>
-        /// <returns>A PaginatedList object</returns>
-        public async Task<PaginatedList<U>> GetPageAsync(T querySet, string route, int page, int pageSize)
+        public static async Task<PagedResult<TDto>> GetPagedResult<TEntity, TDto>(
+            IOrderedQueryable<TEntity> query,
+            int pageNumber,
+            int pageSize,
+            Func<TEntity, TDto> mapper,
+            string route)
+            where TEntity : class
+            where TDto : class
         {
-            // gets one extra element just to make sure that there are more after
-            var data = await querySet
-                .Skip((page - 1) * pageSize)
+
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize+1)
                 .ToListAsync();
-            // hasNext checks if we managed to get one element after the last requested
-            bool hasNext = data.Count > pageSize;
-            //Remove extra element
-            if (hasNext) data.RemoveAt(pageSize);
 
-            return new PaginatedList<U>(data, page, route, hasNext);
+            bool hasNext = items.Count > pageSize;
+
+            return new PagedResult<TDto>
+            {
+                Items = items.Take(pageSize).Select(mapper).ToList(),
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                Next = hasNext ? $"{route}?pageSize={pageSize}&pageNumber={pageNumber+1}": null,
+                Prev = pageNumber > 1 ? $"{route}?pageSize={pageSize}&pageNumber={pageNumber-1}": null,
+            };
         }
+    }
+
+    public class PagedResult<T> where T : class
+    {
+        public List<T> Items { get; set; } = [];
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 0;
+        public string? Next { get; set; } = string.Empty;
+        public string? Prev { get; set; } = string.Empty;
     }
 }
