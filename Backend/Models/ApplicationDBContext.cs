c1883278d14181e999ab68244a512e3ec21ad2ed
Stephane Latil <stephanelatil@gmail.com>
2024-07-25 08:51:45 +0200

    Updates for Moq testing

--- Backend/Models/ApplicationDBContext.cs
+++ Backend/Models/ApplicationDBContext.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 
 namespace Backend.Models
 {
@@ -8,6 +7,9 @@ namespace Backend.Models
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
             : base(options)
+        { }
+        public ApplicationDBContext()
+            : base(new DbContextOptionsBuilder().UseInMemoryDatabase("TestDb").Options)
         { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -32,10 +34,10 @@ namespace Backend.Models
                 .Property(e => e.UniqueHash)
                 .UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
         }
-        public DbSet<Attachment> Attachment { get; set; }
-        public DbSet<EmailAddress> EmailAddress { get; set; }
-        public DbSet<Folder> Folder { get; set; }
-        public DbSet<Mail> Mail { get; set; }
-        public DbSet<MailBox> MailBox { get; set; }
+        public virtual DbSet<Attachment> Attachment { get; set; }
+        public virtual DbSet<EmailAddress> EmailAddress { get; set; }
+        public virtual DbSet<Folder> Folder { get; set; }
+        public virtual DbSet<Mail> Mail { get; set; }
+        public virtual DbSet<MailBox> MailBox { get; set; }
     }
 }
