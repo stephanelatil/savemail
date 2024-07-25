6bce7ba1c0729df07ae97f8c4f79e28f85d313e9
Stephane Latil <stephanelatil@gmail.com>
2024-07-25 10:05:57 +0200

    Adds tests

---
+++ Backend.Tests/ServicesTest/MailBoxServiceTest.cs
@@ -0,0 +1,90 @@
+using Backend.Models;
+using Backend.Models.DTO;
+using Backend.Services;
+using MailKit.Security;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace Backend.Tests.ServicesTest;
+
+public class MailBoxServiceTest
+{
+    private static Mock<ApplicationDBContext> CreateMockContext(params MailBox[] mailboxes){
+        Mock<ApplicationDBContext> context = new();
+        context.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()).Result)
+                    .Returns(1);
+
+        context.SetupGet(c => c.MailBox).ReturnsDbSet(mailboxes);
+        return context;
+    }
+
+    [Fact]
+    public async Task ServiceTest_CreateMailBox()
+    {
+        // Given
+        var context = CreateMockContext();
+        MailBoxService service = new MailBoxService(context.Object);
+        AppUser owner = new (){Id = Guid.Empty.ToString()};
+        UpdateMailBox mb = new(){
+            ImapDomain = "imap.mail.com",
+            ImapPort = 143,
+            Username = "example@mail.com",
+            Password = "password1234",
+            SecureSocketOptions = SecureSocketOptions.Auto,
+            Provider = ImapProvider.Simple
+        };
+
+        // When
+        var record = await Record.ExceptionAsync(
+                                async () => 
+                                await service.CreateMailBoxAsync(mb, owner));
+
+        // Then
+        //no exception thrown
+        Assert.Null(record);
+        //Assert saved to Db
+        context.Verify(c=> c.MailBox.AddAsync(It.IsAny<MailBox>(), It.IsAny<CancellationToken>()));
+        context.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once());
+    }
+
+    [Fact]
+    public async Task ServiceTest_UpdateMailBox()
+    {
+        AppUser owner = new (){Id = Guid.Empty.ToString()};
+        var baseMb = new MailBox(){
+            Id = 123,
+            ImapDomain = "imap.mail.com",
+            ImapPort = 993,
+            Username = "example@mail.com",
+            Password = "password1234",
+            SecureSocketOptions = SecureSocketOptions.Auto,
+            Provider = ImapProvider.Simple
+        };
+        // Given
+        var context = CreateMockContext(baseMb);
+        MailBoxService service = new(context.Object);
+
+        UpdateMailBox mb = new(){
+            Id = baseMb.Id,
+            ImapPort = 999,
+            ImapDomain = "newimap.email.com",
+            Username = "email2@mail.com",
+            Password = "NewP@ssword123",
+            SecureSocketOptions = SecureSocketOptions.None,
+            Provider = ImapProvider.Plain
+        };
+
+        // When
+        var record = await Record.ExceptionAsync(
+                                async () => 
+                                await service.UpdateMailBoxAsync(baseMb.Id, mb));
+
+        // Then
+        //no exception thrown
+        Assert.Null(record);
+        //Assert saved to Db
+        context.VerifyGet(c => c.MailBox, Times.AtLeastOnce());
+        context.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once());
+        context.Verify(c => c.TrackEntry(It.IsAny<MailBox>()), Times.Once());
+    }
+}
