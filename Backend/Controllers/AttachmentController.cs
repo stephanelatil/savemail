28a651e25c134318ad39b8c7df43d94462a5cc44
Stephane Latil <stephane.latil@airbus.com>
2024-07-29 10:49:04 +0200

    Attachment controller now just returns the files

--- Backend/Controllers/AttachmentController.cs
+++ Backend/Controllers/AttachmentController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Backend.Models;
 using Microsoft.AspNetCore.Identity;
+using System.Net.Mime;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Backend.Controllers
 {
@@ -24,88 +27,22 @@ namespace Backend.Controllers
             this._userManager = userManager;
         }
 
-        // GET: api/Attachment
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Attachment>>> GetAttachments()
-        {
-            return await this._context.Attachment.ToListAsync();
-        }
-
-        // GET: api/Attachment/5
+        // GET: api/Attachment/
+        [Authorize]
         [HttpGet("{id}")]
-        public async Task<ActionResult<Attachment>> GetAttachment(long id)
+        public async Task<ActionResult<PhysicalFileResult>> GetAttachment(long id, CancellationToken cancellationToken = default)
         {
-            var attachment = await this._context.Attachment.FindAsync(id);
-
-            if (attachment == null)
-            {
+            var self = await this._userManager.GetUserAsync(this.User);
+            if (self is null || self.Id is null)
+                return this.Forbid();
+            var attachment = await this._context.Attachment.Where(x=>x.Id == id).FirstOrDefaultAsync(cancellationToken);
+            if (attachment is null)
                 return this.NotFound();
-            }
 
-            return attachment;
-        }
+            if (self.Id != attachment?.Owner?.Id)
+                return this.Forbid();
 
-        // PUT: api/Attachment/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAttachment(long id, Attachment attachment)
-        {
-            if (id != attachment.Id)
-            {
-                return this.BadRequest();
-            }
-
-            this._context.TrackEntry(attachment);
-
-            try
-            {
-                await this._context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!this.AttachmentExists(id))
-                {
-                    return this.NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return this.NoContent();
-        }
-
-        // POST: api/Attachment
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Attachment>> PostAttachment(Attachment attachment)
-        {
-            this._context.Attachment.Add(attachment);
-            await this._context.SaveChangesAsync();
-
-            return this.CreatedAtAction("GetAttachment", new { id = attachment.Id }, attachment);
-        }
-
-        // DELETE: api/Attachment/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAttachment(long id)
-        {
-            var attachment = await this._context.Attachment.FindAsync(id);
-            if (attachment == null)
-            {
-                return this.NotFound();
-            }
-
-            this._context.Attachment.Remove(attachment);
-            await this._context.SaveChangesAsync();
-
-            return this.NoContent();
-        }
-
-        private bool AttachmentExists(long id)
-        {
-            return this._context.Attachment.Any(e => e.Id == id);
+            return this.PhysicalFile(attachment.FilePath, "multipart/x-mixed-replace",attachment.FileName);
         }
     }
 }
