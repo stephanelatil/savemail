6bd46aa94e75c4ceba45efbd6de9f3376bfea9d0
Stephane Latil <stephane.latil@airbus.com>
2024-08-01 11:52:56 +0200

    Only returns partial body on folder mails request

--- Backend/Controllers/FolderController.cs
+++ Backend/Controllers/FolderController.cs
@@ -6,97 +6,96 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Backend.Models.DTO;
 
-namespace Backend.Controllers
+namespace Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FolderController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FolderController : ControllerBase
+    private readonly ApplicationDBContext _context;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IFolderService _folderService;
+
+    public FolderController(ApplicationDBContext context,
+                            UserManager<AppUser> userManager,
+                            IFolderService folderService)
     {
-        private readonly ApplicationDBContext _context;
-        private readonly UserManager<AppUser> _userManager;
-        private readonly IFolderService _folderService;
+        this._context = context;
+        this._userManager = userManager;
+        this._folderService = folderService;
+    }
 
-        public FolderController(ApplicationDBContext context,
-                                UserManager<AppUser> userManager,
-                                IFolderService folderService)
+    // GET: api/Folder/5
+    [HttpGet("{id}")]
+    [Authorize]
+    public async Task<ActionResult<FolderDto>> GetFolder(int id)
+    {
+        var folder = await this._context.Folder.Where(f => f.Id == id)
+                                                .Include(f=>f.MailBox)
+                                                .Include(f => f.Parent)
+                                                .Include(f => f.Children)
+                                                .FirstOrDefaultAsync();
+
+        if (folder == null)
         {
-            this._context = context;
-            this._userManager = userManager;
-            this._folderService = folderService;
+            return this.NotFound();
+        }
+        
+        var self = await this._userManager.GetUserAsync(this.User);
+        if (self is null || self.Id is null || folder?.MailBox?.OwnerId != self.Id)
+            return this.Forbid();
+
+        return new FolderDto(folder);
+    }
+
+    // GET: api/Folder/5
+    [HttpGet("{id}/Mails")]
+    [Authorize]
+    public async Task<ActionResult<PaginatedList<MailDto>>> GetMails(int id, PaginationQueryParameters paginationQueryParameters)
+    {
+        var folder = await this._context.Folder.Where(f => f.Id == id)
+                                                .Include(f=>f.MailBox)
+                                                .SingleOrDefaultAsync();
+
+        if (folder == null)
+        {
+            return this.NotFound();
+        }
+        
+        var self = await this._userManager.GetUserAsync(this.User);
+        if (self is null || folder?.MailBox?.Owner != self)
+            return this.Forbid();
+
+        return this.Ok(await PaginationService.GetPagedResult(
+                        this._context.Mail.Where(m => m.FolderId == folder.Id)
+                                                    .OrderByDescending(m =>m.DateSent)
+                                                    .ThenByDescending(m => m.ImapMailUID),
+                        paginationQueryParameters,
+                        (x)=>new MailDto(x, true),
+                        this.Request.PathBase+this.Request.Path
+                        ));
+    }
+
+    // DELETE: api/Folder/5
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<IActionResult> DeleteFolder(int id)
+    {
+        var folder = await this._context.Folder.Where(f => f.Id == id)
+                                                .Include(f=>f.MailBox)
+                                                .SingleOrDefaultAsync();
+        if (folder == null)
+        {
+            return this.NotFound();
         }
 
-        // GET: api/Folder/5
-        [HttpGet("{id}")]
-        [Authorize]
-        public async Task<ActionResult<FolderDto>> GetFolder(int id)
-        {
-            var folder = await this._context.Folder.Where(f => f.Id == id)
-                                                    .Include(f=>f.MailBox)
-                                                    .Include(f => f.Parent)
-                                                    .Include(f => f.Children)
-                                                    .FirstOrDefaultAsync();
+        var self = await this._userManager.GetUserAsync(this.User);
+        if (self is null || folder?.MailBox?.Owner != self)
+            return this.Forbid();
 
-            if (folder == null)
-            {
-                return this.NotFound();
-            }
-            
-            var self = await this._userManager.GetUserAsync(this.User);
-            if (self is null || self.Id is null || folder?.MailBox?.OwnerId != self.Id)
-                return this.Forbid();
+        this._context.Folder.Remove(folder);
+        await this._context.SaveChangesAsync();
 
-            return new FolderDto(folder);
-        }
-
-        // GET: api/Folder/5
-        [HttpGet("{id}/Mails")]
-        [Authorize]
-        public async Task<ActionResult<PaginatedList<MailDto>>> GetMails(int id, PaginationQueryParameters paginationQueryParameters)
-        {
-            var folder = await this._context.Folder.Where(f => f.Id == id)
-                                                    .Include(f=>f.MailBox)
-                                                    .SingleOrDefaultAsync();
-
-            if (folder == null)
-            {
-                return this.NotFound();
-            }
-            
-            var self = await this._userManager.GetUserAsync(this.User);
-            if (self is null || folder?.MailBox?.Owner != self)
-                return this.Forbid();
-
-            return this.Ok(await PaginationService.GetPagedResult(
-                            this._context.Mail.Where(m => m.FolderId == folder.Id)
-                                                        .OrderByDescending(m =>m.DateSent)
-                                                        .ThenByDescending(m => m.ImapMailUID),
-                            paginationQueryParameters,
-                            (x)=>new MailDto(x),
-                            this.Request.PathBase+this.Request.Path
-                            ));
-        }
-
-        // DELETE: api/Folder/5
-        [HttpDelete("{id}")]
-        [Authorize]
-        public async Task<IActionResult> DeleteFolder(int id)
-        {
-            var folder = await this._context.Folder.Where(f => f.Id == id)
-                                                    .Include(f=>f.MailBox)
-                                                    .SingleOrDefaultAsync();
-            if (folder == null)
-            {
-                return this.NotFound();
-            }
-
-            var self = await this._userManager.GetUserAsync(this.User);
-            if (self is null || folder?.MailBox?.Owner != self)
-                return this.Forbid();
-
-            this._context.Folder.Remove(folder);
-            await this._context.SaveChangesAsync();
-
-            return this.NoContent();
-        }
+        return this.NoContent();
     }
 }
--- Backend/Models/DTO/MailDto.cs
+++ Backend/Models/DTO/MailDto.cs
@@ -1,34 +1,42 @@
-using Backend.Models.DTO;
+namespace Backend.Models.DTO;
 
-namespace Backend.Models
+public class MailDto
 {
-    public class MailDto
+    public long Id { get; set; }
+    public long? ReplyTo { get; set; } = null;
+    public ICollection<MailDto> Replies { get; set; } = [];
+    public EmailAddressDto Sender { get; set; } = new EmailAddressDto();
+    public ICollection<EmailAddressDto> Recipients { get; set; } = [];
+    public ICollection<EmailAddressDto> RecipientsCc { get; set; } = [];
+    public string Subject { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public ICollection<AttachmentDto> Attachments { get; set; } = [];
+    public DateTimeOffset DateSent { get; set; } = DateTimeOffset.Now;
+
+    public MailDto(){}
+
+    /// <summary>
+    /// The data transfer object of the Mail object. 
+    /// </summary>
+    /// <param name="mail">The original Mail object</param>
+    /// <param name="fetch_partial">If true only returns part of the body text. Default is false</param>
+    public MailDto(Mail mail, bool fetch_partial=false)
     {
-        public long Id { get; set; }
-        public long? ReplyTo { get; set; } = null;
-        public ICollection<MailDto> Replies { get; set; } = [];
-        public EmailAddressDto Sender { get; set; } = new EmailAddressDto();
-        public ICollection<EmailAddressDto> Recipients { get; set; } = [];
-        public ICollection<EmailAddressDto> RecipientsCc { get; set; } = [];
-        public string Subject { get; set; } = string.Empty;
-        public string Body { get; set; } = string.Empty;
-        public ICollection<AttachmentDto> Attachments { get; set; } = [];
-        public DateTimeOffset DateSent { get; set; } = DateTimeOffset.Now;
-
-        public MailDto(){}
-
-        public MailDto(Mail mail)
+        this.Id = mail.Id;
+        this.ReplyTo = mail.RepliedFrom?.Id;
+        this.Replies = mail.Replies.Select(m => new MailDto(m, fetch_partial)).ToList();
+        this.Sender = mail.Sender is null ? new EmailAddressDto() : new EmailAddressDto(mail.Sender);
+        this.Recipients = mail.Recipients.Select(x => new EmailAddressDto(x)).ToList();
+        this.RecipientsCc = mail.RecipientsCc.Select(x => new EmailAddressDto(x)).ToList();
+        this.Subject = mail.Subject;
+        this.Body = mail.Body;
+        if (fetch_partial)
         {
-            this.Id = mail.Id;
-            this.ReplyTo = mail.RepliedFrom?.Id;
-            this.Replies = mail.Replies.Select(m => new MailDto(m)).ToList();
-            this.Sender = mail.Sender is null ? new EmailAddressDto() : new EmailAddressDto(mail.Sender);
-            this.Recipients = mail.Recipients.Select(x => new EmailAddressDto(x)).ToList();
-            this.RecipientsCc = mail.RecipientsCc.Select(x => new EmailAddressDto(x)).ToList();
-            this.Subject = mail.Subject;
-            this.Body = mail.Body;
-            this.Attachments = mail.Attachments.Select(a => new AttachmentDto(a)).ToList();
-            this.DateSent = mail.DateSent;
+            HtmlAgilityPack.HtmlDocument doc = new();
+            doc.LoadHtml(this.Body);
+            this.Body = doc.DocumentNode.InnerText.Trim().Remove(200);
         }
+        this.Attachments = mail.Attachments.Select(a => new AttachmentDto(a)).ToList();
+        this.DateSent = mail.DateSent;
     }
 }
