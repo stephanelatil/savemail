042d34106c9bc42ae2891fb61fcf0203f411cf9e
Stephane Latil <stephanelatil@gmail.com>
2024-08-17 10:45:47 +0200

    Fixes infinite refetch on rerenders

--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -1,41 +1,33 @@
 'use client'
 
-import { USER_SETTINGS_PAGE } from '@/constants/NavRoutes';
 import { useAppUserData } from '@/hooks/useAppUserData';
 import { useLightDarkModeSwitch } from '@/hooks/useLightDarkModeSwitch';
 import { DarkMode, LightMode, ManageAccounts } from '@mui/icons-material';
 import { CircularProgress, Container, IconButton, Typography } from "@mui/material";
-import { Suspense } from 'react';
-
-const UserNameOrEmail:React.FC = () => {
-    const { getCurrentlyLoggedInUser } = useAppUserData();
-
-    const getName = async() =>
-        {
-            const {email, firstName, lastName} = await getCurrentlyLoggedInUser() ?? {email:"ERROR", firstName:null, lastName:null};
-            const name = `${firstName??''} ${lastName??''}`.trim();
-            return name.length > 1 ? name : email;
-        };
-    
-    return (
-        <Typography noWrap gutterBottom variant="h6">
-            { getName() }
-        </Typography>);
-}
+import { Suspense, useEffect, useMemo, useState } from 'react';
 
 const UserCard :React.FC = () => {
     const { mode, toggleMode } = useLightDarkModeSwitch();
+    const { getCurrentlyLoggedInUser } = useAppUserData();
+    const [username, setUsername] = useState(<CircularProgress key='USERNAME_LOADING'/>)
 
+    useEffect(() =>{
+        async function populateUserNameOrEmail() {
+            const {email, firstName, lastName} = await getCurrentlyLoggedInUser() ?? {email:"ERROR", firstName:null, lastName:null};
+            let name = `${firstName??''} ${lastName??''}`.trim();
+            name = name.length > 1 ? name : email;
+            setUsername(<Typography key='USERNAME'>{ name }</Typography>);
+        }
+        if (username.key === 'USERNAME_LOADING')
+            populateUserNameOrEmail();
+    },[]);
 
     return (
         <Container >
-
-            <IconButton href={USER_SETTINGS_PAGE}>
+            <IconButton href={'/settings'}>
                 <ManageAccounts />
             </IconButton>
-            <Suspense fallback={<CircularProgress />}>
-                <UserNameOrEmail/>
-            </Suspense>
+            { username }
             <IconButton onClick={toggleMode}>
                 {mode === 'light' ? <LightMode /> : <DarkMode />}
             </IconButton>
