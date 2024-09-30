bed2b763dd1cc698e34434656632eb0d627b9016
Stephane Latil <stephanelatil@gmail.com>
2024-09-30 12:47:48 +0200

    Adds migrations

--- Backend/Migrations/20240930103840_ChangesReplyTreeToReplyLinkedList.Designer.cs
+++ Backend/Migrations/20240930103840_ChangesReplyTreeToReplyLinkedList.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -12,9 +13,11 @@ using NpgsqlTypes;
 namespace Backend.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240930103840_ChangesReplyTreeToReplyLinkedList")]
+    partial class ChangesReplyTreeToReplyLinkedList
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Migrations/20240930103840_ChangesReplyTreeToReplyLinkedList.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangesReplyTreeToReplyLinkedList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mail_Mail_RepliedFromId",
+                table: "Mail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Mail_RepliedFromId",
+                table: "Mail");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasReply",
+                table: "Mail",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ReplyId",
+                table: "Mail",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mail_ReplyId",
+                table: "Mail",
+                column: "ReplyId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mail_Mail_ReplyId",
+                table: "Mail",
+                column: "ReplyId",
+                principalTable: "Mail",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mail_Mail_ReplyId",
+                table: "Mail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Mail_ReplyId",
+                table: "Mail");
+
+            migrationBuilder.DropColumn(
+                name: "HasReply",
+                table: "Mail");
+
+            migrationBuilder.DropColumn(
+                name: "ReplyId",
+                table: "Mail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mail_RepliedFromId",
+                table: "Mail",
+                column: "RepliedFromId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mail_Mail_RepliedFromId",
+                table: "Mail",
+                column: "RepliedFromId",
+                principalTable: "Mail",
+                principalColumn: "Id");
+        }
+    }
+}
