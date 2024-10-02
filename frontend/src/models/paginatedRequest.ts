4ab752dcbb68b23c284457967899e86c4f95b32e
Stephane Latil <stephanelatil@gmail.com>
2024-10-02 15:02:01 +0200

    Fixes model

--- frontend/src/models/paginatedRequest.ts
+++ frontend/src/models/paginatedRequest.ts
@@ -3,6 +3,6 @@ import { URL } from "./helpers";
 export interface PaginatedRequest<T>{
     items:T[],
     pageIndex:number,
-    previousPage:URL,
-    nextPage:URL
+    prev:URL,
+    next:URL
 }
