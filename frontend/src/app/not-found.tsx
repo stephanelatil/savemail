32a26d163ad458cef4bb6d7b7abdb9a1727c1a7e
Stephane Latil <stephanelatil@gmail.com>
2024-11-05 09:27:53 +0100

    Adds sidebar for nav on 404

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,7 +1,13 @@
 import NotFound from "@/components/NotFound";
+import Sidebar from "@/components/SideBar";
+import { Stack } from "@mui/material";
 
 const Error404:React.FC = () => {
-    return (<NotFound />);
+    return (
+        <Stack flexDirection='row'>
+          <Sidebar />
+          <NotFound />
+      </Stack>);
 }
 
 export default Error404;
