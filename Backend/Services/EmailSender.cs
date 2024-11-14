e95c8a7795107d525b3eba007eee1dd9d25e7a8c
Stephane Latil <stephanelatil@gmail.com>
2024-11-14 14:34:38 +0100

    Adds Brevo Email sender

--- Backend/Services/EmailSender.cs
+++ Backend/Services/EmailSender.cs
@@ -1,5 +1,6 @@
 namespace Backend.Services;
 
+using System.Text;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using SendGrid;
 using SendGrid.Helpers.Mail;
@@ -30,7 +31,7 @@ public class SendGridEmailSender : IEmailSender
         await this.Execute(subject, message, toEmail);
     }
 
-    public async Task Execute(string subject, string message, string toEmail)
+    private async Task Execute(string subject, string message, string toEmail)
     {
         if (this.fromEmail is null)
             throw new MissingFieldException("SendGrid FromEmail should be set in the config");
@@ -54,6 +55,50 @@ public class SendGridEmailSender : IEmailSender
         else
             this._logger.LogWarning("Failure Email to {} due to {} :\n{}", toEmail, response.StatusCode, response.Body);
     }
+}
 
-    //TODO add method to customize Email with an icon or some styling
+//TODO add MailGun
+
+public class BrevoEmailSender : IEmailSender
+{
+    private readonly string? key = null;
+    private readonly string? senderId = null;
+    private readonly ILogger _logger;
+    private readonly HttpClient _httpClient;
+
+    public BrevoEmailSender(IConfiguration configuration,
+                       ILogger<SendGridEmailSender> logger,
+                       HttpClient httpClient)
+    {
+        this._logger = logger;
+        this.key = configuration.GetValue<string>("Brevo:Key");
+        this.senderId = configuration.GetValue<string>("Brevo:SenderId");
+        this._httpClient = httpClient;
+        this._httpClient.DefaultRequestHeaders.Add("accept", "application/json");
+        this._httpClient.DefaultRequestHeaders.Add("content-type", "application/json");
+        this._httpClient.DefaultRequestHeaders.Add("api-key", this.key);
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        if (this.key is null || this.senderId is null)
+        {
+            this._logger.LogWarning("Cannot send email. Brevo key or SenderId is not set");
+            throw new KeyNotFoundException("Null Brevo Key or SenderId ");
+        }
+        await this.Execute(subject, htmlMessage, email);
+    }
+
+    private async Task Execute(string subject, string message, string toEmail)
+    {
+        string data = $"{{\"sender\":{{\"id\":{this.senderId} }},\"to\":[{{\"email\":\"{toEmail}\"}}],"+
+                      $"\"subject\":\"{subject}\", \"htmlContent\":\"{message}\"}}";
+        HttpContent content = new StringContent(data, Encoding.UTF8, "application/json");
+        var response = await this._httpClient.PostAsync("https://api.brevo.com/v3/smtp/email",
+                                         content);
+        if (response.IsSuccessStatusCode)
+            this._logger.LogDebug("Email to {} queued successfully!", toEmail);
+        else
+            this._logger.LogWarning("Failure Email to {} due to {} :\n{}", toEmail, response.StatusCode, response.Content.ToString());
+    }
 }
