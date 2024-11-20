44b44bc8d514e8f45e6c482621d15ce278bc5118
Stephane Latil <stephanelatil@gmail.com>
2024-11-20 11:09:06 +0100

    Adds fields for attachments

--- Backend/Models/Attachment.cs
+++ Backend/Models/Attachment.cs
@@ -9,10 +9,12 @@ namespace Backend.Models
         public long Id { get; set; }
         [JsonIgnore]
         public Mail? Mail { get; set; }
+        public long? MailId { get; set; }
         public string FileName { get; set; } = string.Empty;
         public long FileSize { get; set; } = 0;
         public string FilePath { get; set; } = string.Empty;
         public AppUser? Owner { get; set; } = null;
+        public string? OwnerId { get; set; } = null;
         public Attachment(){}
     }
 }
