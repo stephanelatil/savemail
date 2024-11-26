c04a6740086863af31cf5c8cc416a713de178098
Stephane Latil <stephanelatil@gmail.com>
2024-11-26 08:40:09 +0100

    Modes extra favicons and manifest

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,6 +17,12 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <meta name="format-detection" content="telephone=no, date=no, email=no, address=no" />
         <body>
           <AppRouterCacheProvider>
