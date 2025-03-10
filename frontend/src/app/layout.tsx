52ac87f0346aea34e189fc67e3c8e13e289ca9ce
Stephane Latil <stephanelatil@gmail.com>
2025-03-10 13:47:47 +0100

    Fixes env not accessible at runtime

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import LightDarkMode from '@/components/LightDarkMode';
 import NotificationSnackbar from '@/components/NotificationSnackbarProvider';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { PublicEnvScript } from 'next-runtime-env';
 
 export const metadata: Metadata = {
   title: 'SaveMail',
@@ -22,6 +23,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest" />
+        <PublicEnvScript />
       </head>
       <meta name="format-detection" content="telephone=no, date=no, email=no, address=no" />
         <body>
--- frontend/src/constants/Envs.tsx
+++ frontend/src/constants/Envs.tsx
@@ -1,2 +1,5 @@
-export const get_frontend_url = ():string => process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
-export const get_backend_url = ():string => process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000";
+import { env } from "next-runtime-env";
+
+
+export const get_frontend_url = ():string => env('NEXT_PUBLIC_FRONTEND_URL') ?? "http://localhost:3000";
+export const get_backend_url = ():string => env('NEXT_PUBLIC_BACKEND_URL') ?? "http://localhost:5000";
